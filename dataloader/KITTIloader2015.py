d72f5ebb64bb1be7fd2834b4b81bc04c2464576f
blackjack201312 <blackjack201303@gmail.com>
2019-04-29 11:12:37 +0000

    add KITTI validation logs.

--- dataloader/KITTIloader2015.py
+++ dataloader/KITTIloader2015.py
@@ -23,8 +23,10 @@ def dataloader(filepath):
 
   image = [img for img in os.listdir(filepath+left_fold) if img.find('_10') > -1]
 
-  train = image[:160]
-  val   = image[160:]
+  #train = image[:160]
+  #val   = image[160:]
+  train = image
+  val   = image
 
   left_train  = [filepath+left_fold+img for img in train]
   right_train = [filepath+right_fold+img for img in train]
