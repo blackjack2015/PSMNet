498ba329526027e3eb139659a311af1a001a967e
blackjack201312 <blackjack201303@gmail.com>
2019-04-29 08:52:01 +0000

    finish validation on KITTI2015 of PSMNet.

--- finetune.py
+++ finetune.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 import argparse
-import os
 import random
 import torch
 import torch.nn as nn
@@ -20,6 +19,7 @@ from dataloader import KITTIloader2015 as ls
 from dataloader import KITTILoader as DA
 
 from models import *
+from settings import *
 
 parser = argparse.ArgumentParser(description='PSMNet')
 parser.add_argument('--maxdisp', type=int ,default=192,
@@ -30,6 +30,8 @@ parser.add_argument('--datatype', default='2015',
                     help='datapath')
 parser.add_argument('--datapath', default='/media/jiaren/ImageNet/data_scene_flow_2015/training/',
                     help='datapath')
+parser.add_argument('--logFile', default='finetune.log',
+                    help='log path')
 parser.add_argument('--epochs', type=int, default=300,
                     help='number of epochs to train')
 parser.add_argument('--loadmodel', default='./trained/submission_model.tar',
@@ -40,7 +42,13 @@ parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='enables CUDA training')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
+
 args = parser.parse_args()
+
+hdlr = logging.FileHandler(args.logFile)
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr) 
+
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 torch.manual_seed(args.seed)
 if args.cuda:
@@ -66,17 +74,17 @@ if args.model == 'stackhourglass':
 elif args.model == 'basic':
     model = basic(args.maxdisp)
 else:
-    print('no model')
+    logger.info('no model')
 
 if args.cuda:
-    model = nn.DataParallel(model)
+    model = nn.DataParallel(model, device_ids=[0, 1, 2, 3])
     model.cuda()
 
 if args.loadmodel is not None:
     state_dict = torch.load(args.loadmodel)
     model.load_state_dict(state_dict['state_dict'])
 
-print('Number of model parameters: {}'.format(sum([p.data.nelement() for p in model.parameters()])))
+logger.info('Number of model parameters: {}'.format(sum([p.data.nelement() for p in model.parameters()])))
 
 optimizer = optim.Adam(model.parameters(), lr=0.1, betas=(0.9, 0.999))
 
@@ -131,14 +139,15 @@ def test(imgL,imgR,disp_true):
         correct = (disp_true[index[0][:], index[1][:], index[2][:]] < 3)|(disp_true[index[0][:], index[1][:], index[2][:]] < true_disp[index[0][:], index[1][:], index[2][:]]*0.05)      
         torch.cuda.empty_cache()
 
-        return 1-(float(torch.sum(correct))/float(len(index[0])))
+        #return 1-(float(torch.sum(correct))/float(len(index[0])))
+        return (float(torch.sum(correct))/float(len(index[0])))
 
 def adjust_learning_rate(optimizer, epoch):
-    if epoch <= 200:
+    if epoch <= 700:
        lr = 0.001
     else:
        lr = 0.0001
-    print(lr)
+    logger.info(lr)
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
@@ -147,6 +156,20 @@ def main():
 	max_acc=0
 	max_epo=0
 	start_full_time = time.time()
+
+        if args.loadmodel is not None:
+	   total_test_loss = 0
+           ## Test ##
+           for batch_idx, (imgL, imgR, disp_L) in enumerate(TestImgLoader):
+               test_loss = test(imgL,imgR, disp_L)
+               logger.info('Iter %d 3-px error in val = %.3f' %(batch_idx, test_loss*100))
+               total_test_loss += test_loss
+
+	   logger.info('epoch 0 total 3-px error in val = %.3f' %(total_test_loss/len(TestImgLoader)*100))
+	   if total_test_loss/len(TestImgLoader)*100 > max_acc:
+		max_acc = total_test_loss/len(TestImgLoader)*100
+	   logger.info('MAX epoch %d total test error = %.3f' %(max_epo, max_acc))
+
 
 	for epoch in range(1, args.epochs+1):
 	   total_train_loss = 0
@@ -158,36 +181,44 @@ def main():
                start_time = time.time() 
 
                loss = train(imgL_crop,imgR_crop, disp_crop_L)
-	       print('Iter %d training loss = %.3f , time = %.2f' %(batch_idx, loss, time.time() - start_time))
+	       logger.info('Iter %d training loss = %.3f , time = %.2f' %(batch_idx, loss, time.time() - start_time))
 	       total_train_loss += loss
-	   print('epoch %d total training loss = %.3f' %(epoch, total_train_loss/len(TrainImgLoader)))
+	   logger.info('epoch %d total training loss = %.3f' %(epoch, total_train_loss/len(TrainImgLoader)))
 	   
                ## Test ##
 
            for batch_idx, (imgL, imgR, disp_L) in enumerate(TestImgLoader):
                test_loss = test(imgL,imgR, disp_L)
-               print('Iter %d 3-px error in val = %.3f' %(batch_idx, test_loss*100))
+               logger.info('Iter %d 3-px error in val = %.3f' %(batch_idx, test_loss*100))
                total_test_loss += test_loss
 
 
-	   print('epoch %d total 3-px error in val = %.3f' %(epoch, total_test_loss/len(TestImgLoader)*100))
+	   logger.info('epoch %d total 3-px error in val = %.3f' %(epoch, total_test_loss/len(TestImgLoader)*100))
 	   if total_test_loss/len(TestImgLoader)*100 > max_acc:
 		max_acc = total_test_loss/len(TestImgLoader)*100
 		max_epo = epoch
-	   print('MAX epoch %d total test error = %.3f' %(max_epo, max_acc))
+	        savefilename = args.savemodel+'best_'+str(epoch)+'.tar'
+	        torch.save({
+	     	    'epoch': epoch,
+	     	    'state_dict': model.state_dict(),
+	     	    'train_loss': total_train_loss/len(TrainImgLoader),
+	     	    'test_loss': total_test_loss/len(TestImgLoader)*100,
+	     	}, savefilename)
+	   logger.info('MAX epoch %d total test error = %.3f' %(max_epo, max_acc))
 
 	   #SAVE
-	   savefilename = args.savemodel+'finetune_'+str(epoch)+'.tar'
-	   torch.save({
-		    'epoch': epoch,
-		    'state_dict': model.state_dict(),
-		    'train_loss': total_train_loss/len(TrainImgLoader),
-		    'test_loss': total_test_loss/len(TestImgLoader)*100,
-		}, savefilename)
+           if epoch % 20 == 0:
+	       savefilename = args.savemodel+'finetune_'+str(epoch)+'.tar'
+	       torch.save({
+	    	    'epoch': epoch,
+	    	    'state_dict': model.state_dict(),
+	    	    'train_loss': total_train_loss/len(TrainImgLoader),
+	    	    'test_loss': total_test_loss/len(TestImgLoader)*100,
+	    	}, savefilename)
 	
-        print('full finetune time = %.2f HR' %((time.time() - start_full_time)/3600))
-	print(max_epo)
-	print(max_acc)
+        logger.info('full finetune time = %.2f HR' %((time.time() - start_full_time)/3600))
+	logger.info(max_epo)
+	logger.info(max_acc)
 
 
 if __name__ == '__main__':
--- run.sh
+++ run.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-python main.py --maxdisp 192 \
-               --model stackhourglass \
-               --datapath dataset/ \
-               --epochs 0 \
-               --loadmodel ./trained/checkpoint_10.tar \
-               --savemodel ./trained/
-
+#python main.py --maxdisp 192 \
+#               --model stackhourglass \
+#               --datapath data/FlyingThings3D_release/ \
+#               --epochs 0 \
+#               --savemodel ./trained/
 
 
 python finetune.py --maxdisp 192 \
                    --model stackhourglass \
                    --datatype 2015 \
-                   --datapath dataset/data_scene_flow_2015/training/ \
-                   --epochs 300 \
-                   --loadmodel ./trained/checkpoint_10.tar \
-                   --savemodel ./trained/
+                   --datapath data/KITTI_release/training/ \
+                   --epochs 0 \
+                   --loadmodel ./trained/pretrained_model_KITTI2015.tar \
+                   --savemodel ./trained/  \
+                   --logFile logs/validate_best.log
 
